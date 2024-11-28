eb20952276fc594981b92bf092bc127a00904c3e
EKF <kostenkovan@yandex.ru>
2024-11-28 14:44:29 +0300

    Update

--- test_autorize.py
+++ test_autorize.py
@@ -10,18 +10,22 @@ list_notifications_only_new = page.get_notifications_only_new(access_token) # з
 have_notification_warning = page.check_status_notification(list_notifications_only_new, "WARNING") # наличие предупредительного уведомления
 page.read_notifications(access_token) # читаем все уведомления, что сбросить стаус "новые"
 
-page.run_script_notifications(access_token) # выполнили скрипт с генерацией уведомления
+#page.run_script_notifications(access_token) # выполнили скрипт с генерацией уведомления
 list_notifications_only_new = page.get_notifications_only_new(access_token) # запрос всех новых уведомлений
 have_notification_alarm = page.check_status_notification(list_notifications_only_new, "ALARM") # наличие уведомления об ошибки
 page.read_notifications(access_token) # читаем все уведомления, что сбросить стаус "новые"
 
 page.run_script_notifications(access_token) # возвращаем скрипт в исходный статус
 
-
+Flag = False
 if have_notification_warning == False:
-    requests.get('https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Не работают уведомления с параметром "Warning"')
-elif have_notification_alarm == False:
-    requests.get('https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Не работают уведомления с параметром "Alarm"')
+    requests.get('https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Не работают уведомления с параметром "Warning"(ТЕСТ)')
 else:
-    requests.get(
-        'https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Автоматический прогон. Не обращайте внимание')
+    Flag = True
+
+if have_notification_alarm == False:
+    requests.get('https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Не работают уведомления с параметром "Alarm"(ТЕСТ)')
+else:
+    if Flag == False:
+        requests.get('https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Автоматический прогон. Не обращайте внимание')
+
