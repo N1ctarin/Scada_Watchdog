32b89cb95e5facffb254f7e7fa1539c0a6f53f59
EKF <kostenkovan@yandex.ru>
2025-04-11 14:55:13 +0300

    Рефакторинг кода

--- test_autorize.py
+++ test_autorize.py
@@ -7,7 +7,7 @@ def notification_warning_and_alarm():
     page = ScadaWatchdogNotification()
     access_token, refresh_token = page.authorization() # получили токены
 
-    for i in range(6):
+    for i in range(6): # Проверка исходных значений в теге (парсит значение на дашборде) - лучше отрефакторить
         status_script = page.check_status_script_pre_run_script(access_token, "0.0")
         if status_script:
             page.read_notifications(access_token)
@@ -17,12 +17,12 @@ def notification_warning_and_alarm():
             time.sleep(2)
 
     page.edit_value_tag(access_token, 6593, "17.0")
-    time.sleep(3)
+    time.sleep(3) # техническое ожидание, чтоб данные успели обновиться в тегах
     list_notifications_only_new = page.get_notifications_only_new(access_token) # запрос всех новых уведомлений
     have_notification_warning = page.check_status_notification(list_notifications_only_new, "WARNING") # наличие предупредительного уведомления
     page.read_notifications(access_token) # читаем все уведомления, что сбросить стаус "новые"
 
-    for i in range(6):
+    for i in range(6): # Проверка исходных значений в теге (парсит значение на дашборде) - лучше отрефакторить
         status_script = page.check_status_script_pre_run_script(access_token, "17.0")
         if status_script:
             page.read_notifications(access_token)
@@ -32,7 +32,7 @@ def notification_warning_and_alarm():
             time.sleep(2)
 
     page.edit_value_tag(access_token, 6593, "25.0")
-    time.sleep(3)
+    time.sleep(3) # техническое ожидание, чтоб данные успели обновиться в тегах
     list_notifications_only_new = page.get_notifications_only_new(access_token) # запрос всех новых уведомлений
     have_notification_alarm = page.check_status_notification(list_notifications_only_new, "ALARM") # наличие уведомления об ошибки
     page.read_notifications(access_token) # читаем все уведомления, что сбросить стаус "новые"
@@ -55,8 +55,8 @@ def check_trigger_script():
     page = ScadaWatchdogNotification()
     access_token, refresh_token = page.authorization() # получили токены
     tags_before_scripts = page.get_group_tags_project(access_token, 1373)  # получили список тегов из папки "скрипты" перед скриптом
-    time.sleep(1)
-    now_value_tag_for_run_script = page.check_value_tag(access_token, 1373, 0)
+    time.sleep(1) # техническое ожидание, чтоб данные успели обновиться в тегах
+    now_value_tag_for_run_script = page.check_value_tag(access_token, 1373, 0) # запоминаем текущее значения в теге
     if now_value_tag_for_run_script == "false":
         value = "true"
     else:
@@ -87,24 +87,9 @@ def check_telegram():
     #    requests.get(
     #        'https://api.telegram.org/bot7205176061:AAGjERufx2q-IAsbHCIAMKEBeHrVyo9lJMo/sendMessage?chat_id=-4503284662&text=Автоматический прогон. Не обращайте внимание')
 
-def proverka():
-    page = ScadaWatchdogNotification()
-    access_token, refresh_token = page.authorization()
-    for i in range(6):
-        status_script = page.check_status_script_pre_run_script(access_token, "0.0")
-        print(status_script)
-        if status_script:
-            page.read_notifications(access_token)
-            break
-        else:
-            page.run_script(access_token, 2533)
-            time.sleep(2)
 
 logger_default.info("START")
 notification_warning_and_alarm()
 check_trigger_script()
 check_telegram()
-logger_default.info(f"Status_code - finish")
-
-#proverka()
-#logger_default.info("FINISH")
+logger_default.info("FINISH")
